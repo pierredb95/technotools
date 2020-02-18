6b2411f26e443e2429c3b9faffaf4f343cce1d1d
Marc Combarel <marc.combarel@edhec.com>
2020-02-18 16:08:33 +0100

    Authentification/ Vinyl controller done /Booking controller work in progress + view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,10 +2,21 @@ class BookingsController < ApplicationController
   # CREATE
   def new
     @booking = Booking.new
+    @vinyl = Vinyl.find(params[:vinyl_id])
   end
 
   def create
 
+    @booking = Booking.new(booking_params)
+    @vinyl = Vinyl.find(params[:vinyl_id])
+    @booking.vinyl = @vinyl
+    @booking.user = current_user
+      raise
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   # READ
@@ -32,5 +43,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
+    params.require(:booking).permit(:start_rent, :end_rent, :vinyl_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :vinyls do
     resources :bookings, except: [ :index, :show, :destroy, :edit, :update]
-    resources :reviews, except: [ :show ]
+    resources :reviews, except: [ :index, :show ]
   end
   resources :bookings, only: [ :index, :show, :destroy, :edit, :update]
 end
