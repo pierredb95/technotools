6ef4510257924055231f5adcedec3dac5cc0d8f6
Basile Masson <basilemasson2@gmail.com>
2020-02-21 14:43:59 +0100

    fixed redirection

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,7 +14,7 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     authorize @booking
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to bookings_path
     else
       render :new
     end
