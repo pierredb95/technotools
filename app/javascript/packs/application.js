5eb1ce7996f74cc594bc7dca8ffa0b79865d948c
Basile Masson <basilemasson2@gmail.com>
2020-02-21 12:40:45 +0100

    on a retrouvé la map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,9 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
-
-initMapbox();
-
-
 import "../plugins/flatpickr"
 import "../plugins/play_music"
 
-import "../plugins/play_music"
+initMapbox();
+
 
--- app/javascript/plugins/play_music.js
+++ app/javascript/plugins/play_music.js
@@ -5,26 +5,32 @@ const video2 = document.querySelector('#youtube2');
 const vinyl3 = document.querySelector('.vinyl3')
 const video3 = document.querySelector('#youtube3');
 
-  // Beatles
-vinyl1.addEventListener('mouseover', (event) => {
-  video1.src = 'https://www.youtube.com/embed/oolpPmuK2I8?start=1&autoplay=1'; // assign url to src property
-});
-vinyl1.addEventListener('mouseleave', (event) => {
-  video1.src = '';
-});
+// Beatles
+if (vinyl1) {
+  vinyl1.addEventListener('mouseover', (event) => {
+    video1.src = 'https://www.youtube.com/embed/oolpPmuK2I8?start=1&autoplay=1'; // assign url to src property
+  });
+  vinyl1.addEventListener('mouseleave', (event) => {
+    video1.src = '';
+  });
+}
 
-  // Michael Jackson
-vinyl2.addEventListener('mouseover', (event) => {
-  video2.src = 'https://www.youtube.com/embed/dIQmcwSg0nU?start=2&autoplay=1'; // assign url to src property
-});
-vinyl2.addEventListener('mouseleave', (event) => {
-  video2.src = '';
-});
+// Michael Jackson
+if (vinyl2) {
+  vinyl2.addEventListener('mouseover', (event) => {
+    video2.src = 'https://www.youtube.com/embed/dIQmcwSg0nU?start=2&autoplay=1'; // assign url to src property
+  });
+  vinyl2.addEventListener('mouseleave', (event) => {
+    video2.src = '';
+  });
+}
 
-  // Rolling Stones
-vinyl3.addEventListener('mouseover', (event) => {
-  video3.src = 'https://www.youtube.com/embed/6HDdiz8MU8o?autoplay=1'; // assign url to src property
-});
-vinyl3.addEventListener('mouseleave', (event) => {
-  video3.src = '';
-});
+// Rolling Stones
+if (vinyl3) {
+  vinyl3.addEventListener('mouseover', (event) => {
+    video3.src = 'https://www.youtube.com/embed/6HDdiz8MU8o?autoplay=1'; // assign url to src property
+  });
+  vinyl3.addEventListener('mouseleave', (event) => {
+    video3.src = '';
+  });
+}
